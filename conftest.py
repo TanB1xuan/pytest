4f9595c4bb36d2bcc9ef3ffb05510e031fa14992
tanbixuan <377437607@qq.com>
2020-02-24 07:03:11 -0800

    study and update conftest.py file's functionality, add test_PyTest_5.py

---
+++ conftest.py
@@ -0,0 +1,12 @@
+#content of conftest.py
+
+
+
+'''
+    record property with custom markers'''
+
+def pytest_collection_modifyitems(session, config, items):
+    for item in items:
+        for marker in item.iter_markers(name = "test_id2"):
+            test_id = marker.args[0]
+            item.user_properties.append(("test_id", test_id))
---
+++ test_PyTest_5.py
@@ -0,0 +1,10 @@
+# content of test_PyTest_5.py
+
+
+
+import pytest
+
+@pytest.mark.test_id2(1502)
+def test_function():
+    assert True
+
